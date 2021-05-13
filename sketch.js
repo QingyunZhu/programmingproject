491ccdfdb27cc6a0ba3ea318b53ef16a35afdab5
QingyunZhu <79492470+QingyunZhu@users.noreply.github.com>
2021-05-12 20:18:13 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,32 +1,38 @@
-var seethru=255 
-//The setup function only happens once 
-function setup(){
-    createCanvas(900,900); //create 500px X 500px canvas
-    background(250,200,255); 
+var jim=0;
+    seethru=255;
+    
+function setup() {
+  createCanvas(600, 600);
 }
 
-//The draw function happens over and over again 
-function draw() { 
-  //the background is purple
-  //circle 
+function draw() {
+  background(250, 200, 255);
+  fill(142, 46, 178,255);//purple
+  stroke(130,40, 130);
+  strokeWeight(18); 
+  ellipse (250, 250, jim, jim); 
+  jim=jim+5;
+  fill(250, 18, 250); 
+
+  rect (mouseX, mouseY, 60, 60);
+  stroke(180, 224, 198);
+  
   stroke(255,178,102); // an RGB color for the circle's border 
   strokeWeight(5);
   fill(102,178,255); // an RGB color for the inside of circle
   ellipse(mouseX,height/2,100,seethru); // center of canvas 20px dia
-  fill(255,255,251, seethru);
-
+  fill(255,0,127);
+  rect (mouseX,mouseY,50,50);
+  fill(255, 255, 251,seethru);
   fill(0,255,0);
   triangle(100,75,58,120,186,175);
   fill(0,0,102);
-  textFont('Helvetic');
+  textFont('helvetica');
   textSize(80);
   textStyle(BOLD);
   text('welcome!',200,200);
   textAlign(CENTER,CENTER);
-
-  fill(255,0,127);
-  rect (mouseX,mouseY,50,50);
-
+  
 }
   
   function mousePressed(){ 
@@ -38,5 +44,8 @@ function draw() {
         }else{ 
 
     seethru=seethru-10;}
+
+
+
 
 }
